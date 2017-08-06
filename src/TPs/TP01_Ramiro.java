d1f088207a0fb93f5d77e1b1d72f49f5d82b54ae
ML <marcelo.lafaille@gmail.com>
2017-08-05 22:18:43 -0300

    Primeros TPs

--- src/TPs/TP01_Ramiro.java
+++ src/TPs/TP01_Ramiro.java
@@ -1,4 +1,23 @@
 package TPs;
 
+/**
+ * 1 . Declara dos variables numéricas (con el valor que desees), muestra por consola la
+ * suma,
+ * resta,
+ * multiplicación,
+ * división y
+ * módulo (resto de la división).
+ *
+ *
+ * 2. Declara 2 variables numéricas (con el valor que desees), e indica cual es mayor de los dos.
+ * Si son iguales indicarlo también. Ves cambiando los valores para comprobar que funciona.
+ *
+ *
+ * 3. Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida por consola.
+ * Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+ *
+ *
+ *4.  Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también debemos indicarlo.
+ */
 public class TP01_Ramiro {
 }
